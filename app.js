7d9123c059029f52ac2d58cdd98e678103624352
Josh Bailey <joshbailey@Annes-MacBook-Pro.local>
2018-04-25 19:02:57 -0700

    allows heroku port

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.get('/game/:num', function(request, response) {
 	response.render('game', {highOrLow: returned});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('Example app listening on port 3000!');
 });
